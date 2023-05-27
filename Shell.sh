fdca2370f179237cfb989a74e5e56de69f51ab3b
Henrydoglass <henrydoglass@yahoo.com>
2023-05-27 04:01:18 +0000

    Script Commit

---
+++ Shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Please Enter Your Name"
+read Name
+echo "$Name, Welcome to OYENOLA's House"
+echo "Please Enter The Door Pin"
+read -s Pin
+echo "Your Pin No. is the Correct Pin, Enter!!!"
